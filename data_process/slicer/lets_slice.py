f4f1cb985e6071b17a746906ec9d133e18a7c149
PoTaTo-Mika <1228427403@qq.com>
2025-07-22 17:17:01 +0800

    fix slice bugs.

--- data_process/slicer/lets_slice.py
+++ data_process/slicer/lets_slice.py
@@ -10,10 +10,14 @@ def process_audio(audio_path, slicer):
     audio_name = audio_path.split('/')[-1].split('.')[0]
     waveform, sr = librosa.load(audio_path, sr=44100)
     chunks = slicer.slice(waveform)
+    
+    # Create sliced directory if it doesn't exist
+    os.makedirs('./data/sliced', exist_ok=True)
+    
     for i, chunk in enumerate(chunks):
         if len(chunk.shape) > 1:
             chunk = chunk.T  # Swap axes if the audio is stereo.
-        soundfile.write(f'{audio_name}_{i}.wav', chunk, sr)  # Save sliced audio files with soundfile.
+        soundfile.write(f'./data/sliced/{audio_name}_{i}.wav', chunk, sr)  # Save sliced audio files with soundfile.
 
 def process_folder(folder_path):
     slicer = Slicer(
@@ -29,5 +33,5 @@ def process_folder(folder_path):
             process_audio(os.path.join(folder_path, file), slicer)
 
 if __name__ == '__main__':
-    folder_path = './data/wav'
+    folder_path = './data'
     process_folder(folder_path)
